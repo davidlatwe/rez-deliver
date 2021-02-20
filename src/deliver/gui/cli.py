16330de21dc95e782c42a8bfba61cedf59429b8c
David Lai <davidlatwe@gmail.com>
2021-02-20 18:44:24 +0800

    module rename

--- src/deliver/gui/cli.py
+++ src/deliver/gui/cli.py
@@ -3,14 +3,14 @@ import os
 import sys
 from .vendor.Qt5 import QtCore, QtWidgets
 from ..cli import load_userconfig as _load_userconfig
-from . import resources, control, view
+from . import resources, control, app
 
 
 def init():
     if sys.platform == "darwin":
         os.environ["QT_MAC_WANTS_LAYER"] = "1"  # MacOS BigSur
 
-    app = QtWidgets.QApplication.instance() or QtWidgets.QApplication([])
+    qapp = QtWidgets.QApplication.instance() or QtWidgets.QApplication([])
 
     storage = QtCore.QSettings(QtCore.QSettings.IniFormat,
                                QtCore.QSettings.UserScope,
@@ -26,16 +26,16 @@ def init():
     qss = resources.load_theme(name=storage.value("theme"))
 
     ctrl = control.Controller(storage)
-    window = view.Window(ctrl=ctrl)
+    window = app.Window(ctrl=ctrl)
     window.setStyleSheet(qss)
 
-    return app, window, ctrl
+    return qapp, window, ctrl
 
 
 def main():
-    app, window, ctrl = init()
+    qapp, window, ctrl = init()
     window.show()
 
     ctrl.defer_search_packages(on_time=200)
 
-    return app.exec_()
+    return qapp.exec_()
