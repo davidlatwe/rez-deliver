c7a1645ea6c4541e465df5f63cf50a39dabf6510
David Lai <davidlatwe@gmail.com>
2021-02-22 20:46:03 +0800

    add test description

--- test/packages/foo/2/package.py
+++ test/packages/foo/2/package.py
@@ -1,6 +1,13 @@
 
 name = "foo"
 
+description = """Title
+This is a long long long long description with multi-lines.
+Second line here, everything is great.
+Lastly, this is an awesome package, keep that in mind.
+Bye.
+"""
+
 version = "2"
 
 build_command = False
