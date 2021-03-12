46c8ca47c0eeadf9307a86c07a8c9c5765548351
David Lai <davidlatwe@gmail.com>
2021-03-13 02:24:10 +0800

    remove helper decorator
    just `pip install -e .` and `rezconfig.plugin_module`

--- src/deliver/rezplugins/application/deliver.py
+++ src/deliver/rezplugins/application/deliver.py
@@ -1,34 +1,10 @@
 """
 Rez developer package delivering tool
 """
-
-import os
-import sys
-import importlib.util  # python 3.5+
 from rez.application import Application
 
 
 command_behavior = {}
-
-
-def ensure_top_module(func):
-    """A decorator to ensure the top module of `rezplugins` is imported
-    """
-    def _ensure_top_module(*args, **kwargs):
-        top_rel = os.path.join(os.path.dirname(__file__), *[".."] * 2)
-        top_dir = os.path.realpath(top_rel)
-        top_name = os.path.basename(top_dir)
-
-        if top_name not in sys.modules:
-            init_py = os.path.join(top_dir, "__init__.py")
-            spec = importlib.util.spec_from_file_location(top_name, init_py)
-            module = importlib.util.module_from_spec(spec)
-            # top_name == spec.name
-            sys.modules[top_name] = module
-
-        func(*args, **kwargs)
-
-    return _ensure_top_module
 
 
 def setup_parser(parser, completions=False):
@@ -52,7 +28,6 @@ def setup_parser(parser, completions=False):
                         help="Print out version of this plugin command.")
 
 
-@ensure_top_module
 def command(opts, parser=None, extra_arg_groups=None):
     from rez.config import config
     from deliver._version import version
