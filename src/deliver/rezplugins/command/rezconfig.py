73de2b7049878b122f0beb642692231e91f58eb1
David Lai <davidlatwe@gmail.com>
2021-04-30 00:19:07 +0800

    prep tests

--- src/deliver/rezplugins/command/rezconfig.py
+++ src/deliver/rezplugins/command/rezconfig.py
@@ -1,12 +1,7 @@
 
-import os as __os
-__root = __os.path.join(__os.path.dirname(__file__), *[".."] * 4)
-
-
 # CONFIG
 deliver = {
     "dev_repository_roots": [
-        __os.path.join(__root, "test", "packages"),
-        __os.path.join(__root, "test", "others"),
+        # you're developer package repository here.
     ],
 }
