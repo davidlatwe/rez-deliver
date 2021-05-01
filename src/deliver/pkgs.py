c026094ca5fcb732cbd0e885a9fbe30fe77f3244
David Lai <davidlatwe@gmail.com>
2021-05-02 03:09:25 +0800

    Add __repr__ for debug

--- src/deliver/pkgs.py
+++ src/deliver/pkgs.py
@@ -306,6 +306,9 @@ class Requested(object):
     def __eq__(self, other):
         return other == (self.name, self.index)
 
+    def __repr__(self):
+        return "Requested(name='%s', index=%r)" % (self.name, self.index)
+
 
 class PackageInstaller(object):
     NotInstalled = 0
